551da0bef74ccb6bdb6c391d8bd80632b61044df
Aiello Chan <aiello.chan@gmail.com>
2021-03-17 17:24:21 +0800

    chro(build): Remove package.sh dep

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-./package.sh
-
 [ ! -r dist ] && mkdir dist
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build *.go
-mv echoo dist/echoo_linux 
+mv echoo dist/echoo_linux
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build *.go
 mv echoo dist/echoo_macos
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build *.go
